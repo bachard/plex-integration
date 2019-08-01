a95af9e82941ed3864d21af979203b972ecb0c28
Bastien Achard <bastien.achard@datadoghq.com>
2019-08-01 17:57:51 +0200

    add baseurl as a parameter

--- plex/datadog_checks/plex/plex.py
+++ plex/datadog_checks/plex/plex.py
@@ -5,14 +5,14 @@ from datadog_checks.base import AgentCheck, ConfigurationError
 
 class PlexCheck(AgentCheck):
     def check(self, instance):
+        baseurl = instance.get("baseurl")
         token = instance.get("token")
         
         if not token:
             raise ConfigurationError("Configuration error, token is missing in plex.yaml")
         
         try:
-            # this should run on the same host as the Plex Server
-            PlexServer(token=token)
+            PlexServer(baseurl=baseurl, token=token)
         except Exception as e:
             self.service_check("plex.server", self.CRITICAL, message=str(e))
         else:
