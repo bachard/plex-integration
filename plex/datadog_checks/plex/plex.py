0c7ed2b0c607c0f94ae0819a1a7ad88e2a1770cd
Bastien Achard <bastien.achard@datadoghq.com>
2019-08-02 13:57:19 +0200

    add movies watched

--- plex/datadog_checks/plex/plex.py
+++ plex/datadog_checks/plex/plex.py
@@ -1,9 +1,11 @@
+from plexapi.library import MovieSection
 from plexapi.server import PlexServer
 
 from datadog_checks.base import AgentCheck, ConfigurationError
 
 
 class PlexCheck(AgentCheck):
+
     def check(self, instance):
         baseurl = instance.get("baseurl")
         token = instance.get("token")
@@ -12,8 +14,19 @@ class PlexCheck(AgentCheck):
             raise ConfigurationError("Configuration error, token is missing in plex.yaml")
         
         try:
-            PlexServer(baseurl=baseurl, token=token)
+            plex = PlexServer(baseurl=baseurl, token=token)
         except Exception as e:
             self.service_check("plex.server", self.CRITICAL, message=str(e))
         else:
             self.service_check("plex.server", self.OK)
+
+        movies_playing_count = 0
+
+        for section in plex.library.sections():
+            if section.TYPE == MovieSection.TYPE:
+                for movie in section.all():
+                    # print(movie.title, movie.isWatched, movie.viewCount)
+                    if movie.isWatched:
+                        movies_playing_count += 1
+
+        self.gauge('plex.movies.playing', movies_playing_count)
